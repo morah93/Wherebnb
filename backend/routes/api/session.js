d7a7e801465f0ae9f646038e55e754d73f0b1e4e
mahbub <mahbub9303@gmail.com>
2022-10-16 21:22:05 -0400

    routes index changed for heroku deployment

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -27,7 +27,7 @@ router.get('/', restoreUser, (req, res) => {
     return res.json({
       ...user.toSafeObject()
     });
-  } else return res.json({})
+  } else return res.json(null)
 });
 
 //login user
