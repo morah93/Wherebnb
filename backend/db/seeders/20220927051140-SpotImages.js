c0320cb57c9726ba15284a9a6cd602de2aa4c9c0
mahbub <mahbub9303@gmail.com>
2022-10-18 18:29:02 -0400

    added spot images to seed

--- backend/db/seeders/20220927051140-SpotImages.js
+++ backend/db/seeders/20220927051140-SpotImages.js
@@ -12,27 +12,52 @@ module.exports = {
     await queryInterface.bulkInsert('SpotImages', [
       {
         spotId: 1,
-        url: 'pic1.com',
+        url: 'https://i.imgur.com/uzNJXom.jpg',//url from imgur
         preview: true
       },
       {
         spotId: 2,
-        url: 'pic2.com',
+        url: 'https://i.imgur.com/ogodRiT.jpg',
         preview: true
       },
       {
         spotId: 3,
-        url: 'pic3.com',
+        url: 'https://i.imgur.com/LQbwHxE.jpg',
         preview: true
       },
       {
         spotId: 4,
-        url: 'pic4.com',
+        url: 'https://i.imgur.com/lvTlgIT.jpg',
         preview: true
       },
       {
         spotId: 5,
-        url: 'pic5.com',
+        url: 'https://i.imgur.com/OrZWwBG.jpg',
+        preview: true
+      },
+      {
+        spotId: 6,
+        url: 'https://i.imgur.com/pSjM433.jpg',
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: 'https://i.imgur.com/mZKvmiu.jpg',
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: 'https://i.imgur.com/7cLUKMA.jpg',
+        preview: true
+      },
+      {
+        spotId: 9,
+        url: 'https://i.imgur.com/TCEnY0u.jpg',
+        preview: true
+      },
+      {
+        spotId: 10,
+        url: 'https://i.imgur.com/lh1bDAa.jpg',
         preview: true
       },
     ], {});
