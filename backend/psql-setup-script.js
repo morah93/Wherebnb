e238bf8188a031db757cd9922200444530a15b13
mahbub <mahbub9303@gmail.com>
2022-12-12 17:49:30 -0500

    SCHEMA

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,7 +1,7 @@
 const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
-  if (!data.incudes(process.env.Schema)) {
-    await sequelize.createSchema(process.env.Schema)
+  if (!data.incudes(process.env.SCHEMA)) {
+    await sequelize.createSchema(process.env.SCHEMA)
   }
 });
